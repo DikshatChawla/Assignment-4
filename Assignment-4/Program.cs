8ded8af5dd7e70e9ba31a6841a2a9a7b03191184
Inderjeet Singh <c0731909@mylambton.ca>
2019-04-17 14:30:28 -0400

    second commit

--- Assignment-4/Program.cs
+++ Assignment-4/Program.cs
@@ -61,4 +61,88 @@ namespace C0732121_a4
             }
 
         }
-       
+        public int FindNumberOfBlankSpaces(string line)
+        {
+            int countletters = 0;
+            int countSpaces = 0;
+            foreach (char c in line)
+            {
+                if (char.IsLetter(c)) { countletters++; }
+                if (char.IsWhiteSpace(c)) { countSpaces++; }
+
+            }
+            return countSpaces;
+        }
+
+        public void lineCount()
+        {
+            string line;
+            TextReader reader = new StreamReader("U:/Users/732121/C0732121-a4/Beowulf.txt");
+            while ((line = reader.ReadLine()) != null)
+            {
+                linecount++;
+            }
+            reader.Close();
+
+            Console.WriteLine(linecount);
+
+        }
+
+        public void countWords()
+        {
+            StreamReader sr = new StreamReader("U:/Users/732121/C0732121-a4/Beowulf.txt");
+            int counter = 0;
+            string delim = ",";
+            string[] fields = null;
+            string line = null;
+            while (!sr.EndOfStream)
+            {
+                line = sr.ReadLine();
+                line.Trim();
+                fields = line.Split(delim.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                counter += fields.Length;
+
+            }
+
+            sr.Close();
+            Console.WriteLine("the number of words in the text file is {0}", counter);
+
+
+        }
+
+        public void Foundnumber()
+        {
+            using (StreamReader file = new StreamReader("U:/Users/732121/C0732121-a4/Beowulf.txt"))
+
+            {
+                int counter = 0;
+
+                int number = 0;
+
+                string line;
+
+                while ((line = file.ReadLine()) != null)
+
+                {
+
+                    if (line.Contains("Sea") && line.Contains("Fare") || line.Contains("sea") && line.Contains("fare"))
+
+                    {
+
+                        int x = counter - 1;
+
+                        number++;
+
+                    }
+
+                    counter++;
+
+                }
+
+                Console.WriteLine($"The number of lines that contains *Sea* and *Fare* are {number}");
+
+                file.Close();
+
+            }
+
+        }
